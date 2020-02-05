9f9eb1a80464ccf187b35a90b5da9dbcc6e3c6fc
linwenjun <linwenjun@163.com>
2020-02-05 02:55:54 +0000

    first commit

---
+++ src/main/java/com/thoughtworks/App.java
@@ -0,0 +1,52 @@
+package com.thoughtworks;
+
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("请点餐（菜品Id x 数量，用逗号隔开）：");
+    String selectedItems = scan.nextLine();
+    String summary = bestCharge(selectedItems);
+    System.out.println(summary);
+  }
+
+  /**
+   * 接收用户选择的菜品和数量，返回计算后的汇总信息
+   *
+   * @param selectedItems 选择的菜品信息
+   */
+  public static String bestCharge(String selectedItems) {
+    // 此处补全代码
+    return selectedItems;
+  }
+
+  /**
+   * 获取每个菜品依次的编号
+   */
+  public static String[] getItemIds() {
+    return new String[]{"ITEM0001", "ITEM0013", "ITEM0022", "ITEM0030"};
+  }
+
+  /**
+   * 获取每个菜品依次的名称
+   */
+  public static String[] getItemNames() {
+    return new String[]{"黄焖鸡", "肉夹馍", "凉皮", "冰粉"};
+  }
+
+  /**
+   * 获取每个菜品依次的价格
+   */
+  public static double[] getItemPrices() {
+    return new double[]{18.00, 6.00, 8.00, 2.00};
+  }
+
+  /**
+   * 获取半价菜品的编号
+   */
+  public static String[] getHalfPriceIds() {
+    return new String[]{"ITEM0001", "ITEM0022"};
+  }
+}
